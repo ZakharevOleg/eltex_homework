82e3d5e76f123460f08de2091993f704b49a0fc9
Zakharev Oleg <siberian3axapka@gmail.com>
2022-07-19 17:09:45 +0700

    calculator with dynamic library

---
+++ 11_calculator_stat_lib/addition.h
@@ -0,0 +1,6 @@
+#ifndef _ADDITION_
+#define _ADDITION_
+
+double addition(double, double);
+
+#endif
---
+++ 11_calculator_stat_lib/multiplication.h
@@ -0,0 +1,7 @@
+#ifndef _MULTIPLICATION_
+#define _MULTIPLICATION_
+
+double multiplication(double, double);
+
+#endif
+
