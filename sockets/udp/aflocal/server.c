767c01ba9ecee5640891b381e33fe00c2d90a5b1
Zakharev Oleg <siberian3axapka@gmail.com>
2022-09-30 15:36:37 +0700

    added udp_aflocal

--- sockets/udp/aflocal/server.c
+++ sockets/udp/aflocal/server.c
@@ -6,24 +6,24 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h>
+#include <sys/un.h>
 
 int main(){
     int server_socket;
-    struct sockaddr_in server_address, client_address;
+    struct sockaddr_un server_address, client_address;
     char server_message[256] = "Hi client!";
     char client_message[256];
     socklen_t client_struct_length = sizeof(client_address);
     
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    server_socket = socket(AF_LOCAL, SOCK_DGRAM, 0);
     
     if (server_socket == -1){
         perror("ERROR opening socket");
         exit(1);
     }
     
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9001);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sun_family = AF_LOCAL;
+    strcpy(server_address.sun_path, "/tmp/mysock.sock");
     
     if(bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1){
         perror("ERROR connecting");
@@ -46,6 +46,7 @@ int main(){
     }
     
     close(server_socket);
+    unlink("/tmp/mysock.sock");
     
     return 0;
 }
