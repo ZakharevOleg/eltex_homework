e6f01b55f63a0fddd0b7b482c201cc21fbae166d
Zakharev Oleg <siberian3axapka@gmail.com>
2022-10-25 22:43:32 +0700

    added broadcast

---
+++ broadcast/reciever.c
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h>
+#include <errno.h>
+
+#define handler_error(str) do{\
+    perror(str);\
+    return 1;}while(0)
+
+int main(void){
+    int recv_socket, mess_len;
+    struct sockaddr_in recv_address;
+    char recv_message[256];
+    socklen_t len = sizeof(struct sockaddr_in);
+
+    recv_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if(recv_socket == -1){
+        handler_error("socket");
+    }
+    
+    
+    recv_address.sin_family = AF_INET;
+    recv_address.sin_port = htons(8888);
+    inet_aton("255.255.255.255", &(recv_address.sin_addr));
+
+    if(bind(recv_socket, (struct sockaddr*)&recv_address, len) == -1){
+        close(recv_socket);
+        handler_error("bind");
+    }
+
+    if((mess_len = recvfrom(recv_socket, recv_message, 255, 0, NULL, 0 )) == -1){
+        close(recv_socket);
+        handler_error("recvfrom");
+    }
+    printf("Received: %s\n", recv_message);
+
+    close(recv_socket);
+    return 0;
+}
