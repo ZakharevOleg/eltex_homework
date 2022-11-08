f41b2b6977b2d373e525c55dec2c5321615d5c8e
Zakharev Oleg <siberian3axapka@gmail.com>
2022-11-08 21:04:28 +0700

    finished unnamed_pipe

--- 15_pipe/unname_pipe.c
+++ 15_pipe/unname_pipe.c
@@ -3,23 +3,61 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
+#define handle_error(str) do{\
+    perror(str);\
+    return 1;}while(0)
 
 int main()
 {
-    
+    int fd1[2];
+    int fd2[2];
+    int pid;
+
+    if (pipe(fd1) == -1){
+        handle_error("pipe");
     }
 
-    int pid;
+    if (pipe(fd2) == -1){
+        handle_error("pipe");
+    }
+
     pid = fork();
     if (pid == -1) {
-
+        handle_error("fork");
     }
-    if (pid == 0) {
+    else if (pid > 0) {
+        char mess1[10];
+        char mess[] = "Hello";
+
+        close(fd1[0]);
+        write(fd1[1], mess, strlen(mess) + 1);
+        close(fd1[1]);
+
+        wait(NULL);
+
+        close(fd2[1]);
+        read(fd2[0], mess1, 10);
+        printf("Received message from child: %s\n", mess1);
+        close(fd2[0]);
+
+        printf("Ok\n");
+        return 0;
     }
+    else {
+        char mes1[10];
+        char mes[] ="Hi";
 
-    wait(NULL);
+        close(fd1[1]);
+        read(fd1[0], mes1, 10);
+        printf("Received message from parent: %s\n", mes1);
+        close(fd1[0]);
+        
+        close(fd2[0]);
+        write(fd2[1], mes, strlen(mes) + 1);
+        close(fd2[1]);
 
-    printf("Ok\n");
-    return 0;
+        exit(0);
+    }
 }
